85673195ce9fb45e9fd60abe2d6482403acb3bd8
dionigi <d.marzucca@gmail.com>
2020-12-18 10:05:30 +0100

    Add custom application to init stuff

---
+++ app/src/main/java/com/skytron/netresultspushnotification/application/NetResultsApplication.java
@@ -0,0 +1,11 @@
+package com.skytron.netresultspushnotification.application;
+
+import android.app.Application;
+
+public class NetResultsApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
